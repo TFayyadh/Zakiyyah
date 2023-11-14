e79ae332b3f96aaede0eba7aff85bdaae760b633
TFayyadh <tohahalim@gmail.com>
2023-11-14 23:29:39 +0800

    Create Navbar
    Logo, Zakiyyah name, Home, About Us, Services, Contact Us and Donate button.

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./node_modules/flowbite/**/*.js", "./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('flowbite/plugin')],
+}
+
